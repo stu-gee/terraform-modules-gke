95457c798d6be67b6db090e41446347db7e84634
Stuart Gendron <stuart.gendron@arctiq.ca>
2020-12-22 16:36:23 +0000

    changed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "gke1" {
   source  = "tfe.stugendron.2741611427.workshops.arctiq.ca/arctiq/gke/google"
-  version = "1.0.0"
+  version = "1.0.1"
  
   # insert required variables here
   cluster_name = var.cluster_name
